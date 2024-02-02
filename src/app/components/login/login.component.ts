7aaf92058a110898a1754662f64a1e2383473134
Matheus Dias <matheus.silva.dias@g.globo>
2024-02-02 00:02:15 -0300

    login após criar a conta

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -24,29 +24,34 @@ export class LoginComponent {
   showLoginForm: boolean = true;
   showCreateAccountForm: boolean = false;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) { }
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
 
   ngOnInit() {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
     });
 
-    this.createAccountForm = this.fb.group({
-      name: ['', Validators.required],
-      surname: ['', Validators.required],
-      birth: ['', Validators.required],
-      newUsername: ['', [Validators.required, Validators.email]],
-      newPassword: ['', Validators.required],
-      confirmPassword: ['', Validators.required],
-    }, { validators: this.passwordsMatchValidator });
+    this.createAccountForm = this.fb.group(
+      {
+        name: ['', Validators.required],
+        surname: ['', Validators.required],
+        birth: ['', Validators.required],
+        newUsername: ['', [Validators.required, Validators.email]],
+        newPassword: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+      },
+      { validators: this.passwordsMatchValidator }
+    );
   }
 
-  passwordsMatchValidator(control: AbstractControl): { [key: string]: boolean } | null {
+  passwordsMatchValidator(
+    control: AbstractControl
+  ): { [key: string]: boolean } | null {
     const newPassword = control.get('newPassword')?.value;
     const confirmPassword = control.get('confirmPassword')?.value;
 
-    return newPassword === confirmPassword ? null : { 'passwordsNotMatch': true };
+    return newPassword === confirmPassword ? null : { passwordsNotMatch: true };
   }
 
   toggleForms() {
@@ -67,7 +72,31 @@ export class LoginComponent {
   }
 
   onCreateAccountSubmit() {
-    this.authService.createAccount(this.createAccountForm)
-    console.log("Create account form submitted");
+    this.authService
+      .createAccount(this.createAccountForm)
+      .subscribe((result) => {
+        if (result.error) {
+          console.error(result.error);
+        } else {
+          //console.log(result);
+        }
+      });
+    console.log('Create account form submitted');
+
+    const { newUsername, newPassword } = this.createAccountForm.value;
+    const loginCredentials = this.fb.group({
+      username: [newUsername, Validators.required],
+      password: [newPassword, Validators.required],
+    });
+
+    this.authService
+      .authenticateUser(loginCredentials)
+      .subscribe((loginResult) => {
+        if (loginResult.error) {
+          console.error(loginResult.error);
+        } else {
+          console.log('Login bem sucedido após criar conta:', loginResult);
+        }
+      });
   }
 }
